b4feb86e6dee813000c909aa414ce5374a88316f
gcnova <zhangnova927@foxmail.com>
2022-06-09 17:48:09 +0800

    feat:添加mongoose模块

---
+++ model/db.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test",{ useNewUrlParser: true }),function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('数据库连接成功');
+};
+
+module.exports = mongoose
